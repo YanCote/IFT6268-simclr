9ed7a780053ca7ea608e4aeda2a5a496867ed30a
Yan Cote <ycote@apple.com>
2020-10-04 23:02:24 -0400

    First readme file

--- deployment/imclass.sh
+++ deployment/imclass.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --time=00:30:00
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
 #SBATCH --account=def-bengioy
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32000M
