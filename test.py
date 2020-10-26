9703566381709d0ecd9711b9364b6d82924dcac8
Marc-Andre Ruel <maruel@cedar1.cedar.computecanada.ca>
2020-10-26 07:57:33 -0700

    [#12] Bug fixing

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     use_cache = False
     data_frame_path = "~/scratch/data/Data_Entry_2017.csv"
-    img_data_path = "~/scratch/data/chest-xray/images-224"
+    img_data_path = "~/scratch/data/images-224"
     config = dict()
     scratch_dir = None
     batch_size = 128
